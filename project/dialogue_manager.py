ade593ae3e05de48f5e02c77a411bb9e92d2587e
Jean-Claude Manoli <jcmanoli@figeas.ch>
2018-04-24 13:09:33 +0200

    Implemented thread ranker.

--- project/dialogue_manager.py
+++ project/dialogue_manager.py
@@ -21,11 +21,14 @@ class ThreadRanker(object):
         """
         thread_ids, thread_embeddings = self.__load_embeddings_by_tag(tag_name)
 
+        print((question, tag_name))
+        print((thread_ids.shape, thread_embeddings.shape))
+
         # HINT: you have already implemented a similar routine in the 3rd assignment.
-        
-        question_vec = #### YOUR CODE HERE ####
-        best_thread = #### YOUR CODE HERE ####
-        
+        question_vec = question_to_vec(question, self.word_embeddings, self.embeddings_dim)
+        best_thread = pairwise_distances_argmin([question_vec], thread_embeddings, metric='cosine')[0]
+
+        print(best_thread)
         return thread_ids[best_thread]
 
 
@@ -54,6 +57,7 @@ class DialogueManager(object):
         ########################
         #### YOUR CODE HERE ####
         ########################
+        pass
        
     def generate_answer(self, question):
         """Combines stackoverflow and chitchat parts using intent recognition."""
@@ -61,23 +65,23 @@ class DialogueManager(object):
         # Recognize intent of the question using `intent_recognizer`.
         # Don't forget to prepare question and calculate features for the question.
         
-        prepared_question = #### YOUR CODE HERE ####
-        features = #### YOUR CODE HERE ####
-        intent = #### YOUR CODE HERE ####
+        prepared_question = text_prepare(question)
+        features = self.tfidf_vectorizer.transform([prepared_question,])
+        intent = self.intent_recognizer.predict(features)[0]
 
         # Chit-chat part:   
         if intent == 'dialogue':
             # Pass question to chitchat_bot to generate a response.       
-            response = #### YOUR CODE HERE ####
+            response = "I'm not in the mood..."  #### YOUR CODE HERE ####
             return response
         
         # Goal-oriented part:
         else:        
             # Pass features to tag_classifier to get predictions.
-            tag = #### YOUR CODE HERE ####
+            tag = self.tag_classifier.predict(features)[0]
             
             # Pass prepared_question to thread_ranker to get predictions.
-            thread_id = #### YOUR CODE HERE ####
+            thread_id = self.thread_ranker.get_best_thread(prepared_question, tag)
            
             return self.ANSWER_TEMPLATE % (tag, thread_id)
 
